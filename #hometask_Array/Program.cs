e556d9fb6d33f84712efd00a6eb8ae38a6dfff85
nihadaslanli <nihadsasl@code.edu.az>
2024-02-17 19:43:23 +0400

    add

--- #hometask_Array/Program.cs
+++ #hometask_Array/Program.cs
@@ -3,15 +3,15 @@ Console.WriteLine("Hello, World!");
 #region task5
 
 
-int[] num =new int[10];
+//int[] num =new int[10];
 
-for (int i = 0; i < num.Length; i++)
-{
-    Console.WriteLine("reqemleri yazin" + (i + 1));
-    num[i] = Convert.ToInt32(Console.WriteLine());
+//for (int i = 0; i < num.Length; i++)
+//{
+//    Console.WriteLine("reqemleri yazin" + (i + 1));
+//    num[i] = Convert.ToInt32(Console.WriteLine());
 
-}
-Console.WriteLine(*********);
+//}
+//Console.WriteLine(*********);
 
 
 
