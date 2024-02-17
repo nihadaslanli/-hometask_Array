61459a4d706c79b96f1d0a405eae11fc1514c425
nihadaslanli <nihadsasl@code.edu.az>
2024-02-17 18:51:48 +0400

    Add project files.

---
+++ #hometask_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region task5
+
+
+int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+for (int i = 0; i < num.Length; i++)
+{
+    Console.WriteLine(num[i]);
+
+}
+#endregion
+
+
