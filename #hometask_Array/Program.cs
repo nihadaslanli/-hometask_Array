c432ebdf85c216b1f640f4d9427ad6267ffb3697
nihadaslanli <nihadsasl@code.edu.az>
2024-02-17 19:42:45 +0400

    add somthink

--- #hometask_Array/Program.cs
+++ #hometask_Array/Program.cs
@@ -3,12 +3,66 @@ Console.WriteLine("Hello, World!");
 #region task5
 
 
-int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int[] num =new int[10];
+
 for (int i = 0; i < num.Length; i++)
 {
-    Console.WriteLine(num[i]);
+    Console.WriteLine("reqemleri yazin" + (i + 1));
+    num[i] = Convert.ToInt32(Console.WriteLine());
 
 }
+Console.WriteLine(*********);
+
+
+
+
+#endregion
+#region task6
+
+
+
+
+
+
+
+
+
+
+
+#endregion
+
+
+#region task7
+
+
+//int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+
+
+//for (int i = 0; i < num.Length; i++) 
+//{
+//    if (num[i] >= 0) 
+//}
+//{
+
+//    Console.WriteLine(sum[0]);
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endregion
 
 
