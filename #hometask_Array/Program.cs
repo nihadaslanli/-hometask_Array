9dd117269150d58f8e27f7441289d8fb39775544
nihadaslanli <nihadsasl@code.edu.az>
2024-02-19 00:07:38 +0400

    add

--- #hometask_Array/Program.cs
+++ #hometask_Array/Program.cs
@@ -2,37 +2,82 @@
 Console.WriteLine("Hello, World!");
 #region task5
 
+//int[] num = { 2, 4, 7, 6, 9, 10 };
 
-//int[] num =new int[10];
+//int sum = 0;
 
 //for (int i = 0; i < num.Length; i++)
 //{
-//    Console.WriteLine("reqemleri yazin" + (i + 1));
+//    sum += num[i];
+//}
+//Console.WriteLine(sum);
+
+
+
+
+
+
+
+//int[] num = new int[10];
+
+//for (int i = 0; i < num.Length; i++)
+//{
+//    Console.WriteLine("reqemleri yazin" + ("i + 1"));
 //    num[i] = Convert.ToInt32(Console.WriteLine());
 
 //}
-//Console.WriteLine(*********);
+//Console.WriteLine(num[i]);
 
 
 
+//Console.Write("istenilen musbet ededi daxil edin:");
 
-#endregion
-#region task6
+//int n = Convert.ToInt32(Console.ReadLine());
+//bool isLoop = true;
+//while (isLoop) 
+//{
 
-
-
-
-
-
-
-
-
+//}
 
 
 #endregion
 
 
 #region task7
+Console.WriteLine("istenilen ededleri qeyd edin");
+int[]num=Convert.ToInt32(Console.WriteLine(""));
+int sum = 0;
+for (int i = 0; i < 100; i++)
+
+{
+	while (true) 
+	{
+		sum++;
+
+	}
+}
+{ Console.WriteLine(sum); }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
