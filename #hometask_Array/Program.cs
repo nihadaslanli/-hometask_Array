73d0921ad7cf802bdd5b61e549b8381e3cc7b163
nihadaslanli <nihadsasl@code.edu.az>
2024-02-19 13:58:46 +0400

    feat;add anything

--- #hometask_Array/Program.cs
+++ #hometask_Array/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 #region task5
-
 //int[] num = { 2, 4, 7, 6, 9, 10 };
 
 //int sum = 0;
@@ -11,102 +10,31 @@ Console.WriteLine("Hello, World!");
 //    sum += num[i];
 //}
 //Console.WriteLine(sum);
+#endregion
+#region task6
 
+int[] num = { 10,12,14,24,35,45};
+int n = num[i];
 
-
-
-
-
-
-//int[] num = new int[10];
-
-//for (int i = 0; i < num.Length; i++)
-//{
-//    Console.WriteLine("reqemleri yazin" + ("i + 1"));
-//    num[i] = Convert.ToInt32(Console.WriteLine());
-
-//}
-//Console.WriteLine(num[i]);
-
-
-
-//Console.Write("istenilen musbet ededi daxil edin:");
-
-//int n = Convert.ToInt32(Console.ReadLine());
-//bool isLoop = true;
-//while (isLoop) 
-//{
-
-//}
+for (int i = 1; i < num.Length; i++)
+{
+    if (n > num[i])
+    {
+       
+    }
+}
+Console.WriteLine(n);
 
 
 #endregion
 
-
 #region task7
-Console.WriteLine("istenilen ededleri qeyd edin");
-int[]num=Convert.ToInt32(Console.WriteLine(""));
-int sum = 0;
-for (int i = 0; i < 100; i++)
-
-{
-	while (true) 
-	{
-		sum++;
-
-	}
-}
-{ Console.WriteLine(sum); }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
+//int sum = num[0] + num[ num.Length - 1];
 
-
-
-//for (int i = 0; i < num.Length; i++)
-//{
-//    if (num[i] >= 0)
-//}
-//{
-
-//    Console.WriteLine(sum[0]);
-
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Console.WriteLine(sum);
 
 #endregion
 
